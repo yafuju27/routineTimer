c508d0bac9f89d0f3f357b1d9b8c11771edf8dac
yafuju27 <yafuju@gmail.com>
2022-03-24 18:25:51 +0900

    clean

--- routineTimer/Controller/SecondViewController.swift
+++ routineTimer/Controller/SecondViewController.swift
@@ -9,14 +9,12 @@
 import UIKit
 import RealmSwift
 
-class SecondViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UICollectionViewDragDelegate, UICollectionViewDropDelegate {
-    
+class SecondViewController: UIViewController {
     @IBOutlet weak var taskList: UICollectionView!
     @IBOutlet weak var startButton: UIButton!
     @IBOutlet weak var saveButton: UIButton!
     @IBOutlet weak var titleTextField: UITextField!
     
-
     @IBOutlet weak var bellButton: UIButton!
     
     private var viewWidth: CGFloat!
@@ -26,21 +24,60 @@ class SecondViewController: UIViewController, UICollectionViewDelegate, UICollec
     private var cellOffset: CGFloat!
     private var navHeight: CGFloat!
     
-    var taskArray = ["トイレ","うがい","水分補給","体重測定","スクワット","ストレッチ","英単語","Todoリスト確認","トイレ","うがい","水分補給","体重測定","スクワット","ストレッチ","英単語","Todoリスト確認","トイレ","うがい","水分補給","体重測定","スクワット","ストレッチ","英単語","Todoリスト確認"]
-    var taskTimeArray = ["5分30秒","2分00秒","1分00秒","3分00秒","4分30秒","2分20秒","0分30秒","1分20秒","5分30秒","2分00秒","1分00秒","3分00秒","4分30秒","2分20秒","0分30秒","1分20秒","5分30秒","2分00秒","1分00秒","3分00秒","4分30秒","2分20秒","0分30秒","1分20秒"]
-    
-    
-    //var imageOfIcon = iconImageView.imageView
+    var taskArray = ["トイレ"]
+    var taskTimeArray = ["5分30秒"]
     var selectedImage: UIImage!
-    
-    //Realmを使う時のお決まりのやつ
     let realm = try! Realm()
-    
-    //---------------------------------------------------------------------------------------
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupView()
+        
+    }
+    
+    @IBAction func startButton(_ sender: Any) {
+        //ボタンの振動
+        Feedbacker.impact(style: .medium)
+    }
+    @IBAction func saveButton(_ sender: Any) {
+        
+        //ボタンを押したら、先ほど用意したデータの箱に、テキストフィールドに入力された値を書き込む処理を追記。
+        let routine = Routine()
+        routine.title = titleTextField.text!
+        try! realm.write {
+            realm.add(routine)
+        }
+        
+        //ViewControllerへ戻る処理
+        self.navigationController?.popViewController(animated: true)
+        //ボタンの振動
+        Feedbacker.impact(style: .medium)
+    }
+    
+    
+    
+    @IBAction func addTaskButtonAction(_ sender: Any) {
+    }
+    
+    
+    @IBAction func bellButtonAction(_ sender: Any) {
+        //ボタンの振動
+        Feedbacker.impact(style: .medium)
+    }
+    
+    //キーボード閉じる処理
+    @objc func dismissKeyboard() {
+        self.view.endEditing(true)
+    }
+    
+    private func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.titleTextField.resignFirstResponder()
+        return true
+    }
+    
+    
+    private func setupView() {
         startButton.layer.cornerRadius = 12
         startButton.layer.shadowOpacity = 0.2
         startButton.layer.shadowRadius = 12
@@ -82,17 +119,22 @@ class SecondViewController: UIViewController, UICollectionViewDelegate, UICollec
         let routineItems = realm.objects(Routine.self)
         print("🟥全てのデータ\(routineItems)")
     }
-    //---------------------------------------------------------------------------------------
     
-    //キーボード閉じる処理
-    @objc func dismissKeyboard() {
-        self.view.endEditing(true)
-    }
-    
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        self.titleTextField.resignFirstResponder()
-        return true
-    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+extension SecondViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
     //セルの個数
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -128,91 +170,42 @@ class SecondViewController: UIViewController, UICollectionViewDelegate, UICollec
     //            performSegue(withIdentifier: "toSecondViewController",sender: nil)
     //        }
     
+    
+}
+
+
+extension SecondViewController: UICollectionViewDragDelegate, UICollectionViewDropDelegate {
+    
     //セルのドラッグ
     func collectionView(_ collectionView: UICollectionView, itemsForBeginning session: UIDragSession, at indexPath: IndexPath) -> [UIDragItem] {
         let itemIdentifier = indexPath.item.description
-                let itemProvider = NSItemProvider(object: itemIdentifier as NSItemProviderWriting)
-                let dragItem = UIDragItem(itemProvider: itemProvider)
-                return [dragItem]
+        let itemProvider = NSItemProvider(object: itemIdentifier as NSItemProviderWriting)
+        let dragItem = UIDragItem(itemProvider: itemProvider)
+        return [dragItem]
     }
     
     //謎
     func collectionView(_ collectionView: UICollectionView, dropSessionDidUpdate session: UIDropSession, withDestinationIndexPath destinationIndexPath: IndexPath?) -> UICollectionViewDropProposal {
-            return UICollectionViewDropProposal(operation: .move, intent: .insertAtDestinationIndexPath)
-        }
+        return UICollectionViewDropProposal(operation: .move, intent: .insertAtDestinationIndexPath)
+    }
     //セルのドロップ
     func collectionView(_ collectionView: UICollectionView, performDropWith coordinator: UICollectionViewDropCoordinator) {
         if coordinator.proposal.operation == .move {
-                    guard let item = coordinator.items.first,
-                          let destinationIndexPath = coordinator.destinationIndexPath,
-                          let sourceIndexPath = item.sourceIndexPath else {
-                        return
-                    }
-
-                    collectionView.performBatchUpdates({
-//                        // データソースの更新
-//                        let n = dataList.remove(at: sourceIndexPath.item)
-//                        dataList.insert(n, at: destinationIndexPath.item)
-                        //セルの移動
-                        collectionView.deleteItems(at: [sourceIndexPath])
-                        collectionView.insertItems(at: [destinationIndexPath])
-                    })
-                    coordinator.drop(item.dragItem, toItemAt: destinationIndexPath)
+            guard let item = coordinator.items.first,
+                  let destinationIndexPath = coordinator.destinationIndexPath,
+                  let sourceIndexPath = item.sourceIndexPath else {
+                return
             }
-    }
-    
-
-    //------------------------------------
-    @IBAction func startButton(_ sender: Any) {
-        //ボタンの振動
-        Feedbacker.impact(style: .medium)
-    }
-    @IBAction func saveButton(_ sender: Any) {
-        
-        //ボタンを押したら、先ほど用意したデータの箱に、テキストフィールドに入力された値を書き込む処理を追記。
-        let routine = Routine()
-        routine.title = titleTextField.text!
-//        routine.time = timeBox.text!
-//        routine.number = numberBox.text!
-        //routine.color = optionColor
-        try! realm.write {
-            realm.add(routine)
+            
+            collectionView.performBatchUpdates({
+                // データソースの更新
+                // let n = dataList.remove(at: sourceIndexPath.item)
+                // dataList.insert(n, at: destinationIndexPath.item)
+                //セルの移動
+                collectionView.deleteItems(at: [sourceIndexPath])
+                collectionView.insertItems(at: [destinationIndexPath])
+            })
+            coordinator.drop(item.dragItem, toItemAt: destinationIndexPath)
         }
-        
-        //ViewControllerへ戻る処理
-        self.navigationController?.popViewController(animated: true)
-//        //(タイトル、日付、本文)のセットである「日記」を配列の中に入れる
-//        //date,body,date
-//        print("セーブボタン押した")
-//        contentsArray = saveGetModel.getData()
-//        //「DateModel」で定義した設計図をインスタンスにする
-//        let dateModel = DateModel()
-//        //構造体の設計図をインスタンスとして定義する
-//        //左の「contentsModel」はインスタンス、右の「ContentsModel」は設計図
-//        //構造体はインスタンスにしなければ使用することができない
-//        let routinesModel = RoutinesModel(title: titleTextField.text!,body:honbunTextView.text!, date: dateModel.getTodayDate())
-//        print("🟥\(routinesModel)")
-//        //インスタンス化された「ContentsModel()」を「contentsArray」に入れる
-//        contentsArray.append(routinesModel)
-//        //「saveGetModel」の「backProtocol」を自分に委任する
-//        saveGetModel.backProtocol = self
-//        //保存をする(モデルに渡す)
-//        //「SveGetModel.swift」で作成したメソッド「saveData」を発動
-//        //saveData()を発動したことにより、データを保存する
-//        saveGetModel.saveData(contentsArray: contentsArray)
-        
-        //ボタンの振動
-        Feedbacker.impact(style: .medium)
-    }
-
-    
-    
-    @IBAction func addTaskButtonAction(_ sender: Any) {
-    }
-    
-    
-    @IBAction func bellButtonAction(_ sender: Any) {
-        //ボタンの振動
-        Feedbacker.impact(style: .medium)
     }
 }
